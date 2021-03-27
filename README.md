64f0ad58af5d889b045a0d5023f8c65f038d3ffe
Duke Stank <dukestank@dukes-mbp.lan>
2021-03-27 13:29:30 -0400

    Finalized README.md

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Lab08
+# SY402 Lab 08
+
+This Python file 'hash.py' allows users to record and monitor changes in the file structure.
+
+How To Use:
+- Download 'hash.py' into a folder for use.
+- Store the initial hash with the following command. This will scrape all the filenames and hashes associated inside that folder and store them in a timestamped log file such as **131023-27Mar2021.SY402Log**
+
+        python3 hash.py -s ~/Desktop/important_folder
+- You can now use that folder normally, adding, deleted, and editing files within that **important_folder**. 
+- When you want to monitor the additions, modifications, and removals of files in that folder, use the following command.
+ 
+        python3 hash.py -c 131023-27Mar2021.SY402Log ~/Desktop/important_folder
+- This will create a resultant log file that states the files which have been added, modified, or removed. For example:
+
+        FOUND: []
+        MODIFIED: ["~/Desktop/important_folder/file1.txt"]
+        DELETED: []
+        ADDED: ["~/Desktop/important_folder/file2.txt"]
+- Moreover, the console will print an output that states the numbers of changes.
+
+        >> Found: 0 Old Files
+        >> Found: 0 Modified Files
+        >> Found: 1 Deleted Files
+        >> Found: 1 New Files
--- hash.py
+++ hash.py
@@ -13,10 +13,21 @@ import json
 
 
 ignore_dir = ["/usr", "/boot", "/bin", "/etc", "/dev", "/proc", "/run", "/sys", "/tmp", "/var/lib", "/var/run"]
-    
+
+json_out = []
+json_compare = []
+
+def nested_output(result, hash, outfile):
+    now = datetime.now().strftime("%H%M%S-%d%b%Y")
+
+    #f = open(outfile, "a")
+    #f.write(json.dumps({'filename' : result, "hash" : hash, "DT":now}))
+    #print(result)
+    json_out.append({'filename' : result, "hash" : hash, "datetime":now})
+    #f.close()
 
 def part1(argv):
-    for (dirpath, dirnames, filenames) in os.walk(argv[1], topdown=True):
+    for (dirpath, dirnames, filenames) in os.walk(argv[2], topdown=True):
         if(dirpath not in ignore_dir):
             for f in filenames:
                 print(os.path.join(dirpath, f))
@@ -24,7 +35,7 @@ def part1(argv):
                 print(os.path.join(dirpath, d))
 
 def part2(argv):
-    for (dirpath, dirnames, filenames) in os.walk(argv[1], topdown=True):
+    for (dirpath, dirnames, filenames) in os.walk(argv[2], topdown=True):
         if(dirpath not in ignore_dir):
             for f in filenames:
                 result = os.path.join(dirpath, f)
@@ -37,22 +48,95 @@ def part2(argv):
 
 def part3(argv,outfilename):
     out = []
-    for (dirpath, dirnames, filenames) in os.walk(argv[1], topdown=True):
+    for (dirpath, dirnames, filenames) in os.walk(argv[2], topdown=True):
         if(dirpath not in ignore_dir):
             for f in filenames:
                 result = os.path.join(dirpath, f)
                 #print(result)
                 #print(hashlib.sha256(open(result,'rb').read()).hexdigest())
-                nested_output(result,hashlib.sha256(open(result,'rb').read()).hexdigest())
+                nested_output(result,hashlib.sha256(open(result,'rb').read()).hexdigest(),outfilename)
             for d in dirnames:
                 result = os.path.join(dirpath, d)
                 #print(result)
                 #print(hashlib.sha256(result.encode()).hexdigest())
-                nested_output(result,hashlib.sha256(result.encode()).hexdigest())
+                nested_output(result,hashlib.sha256(result.encode()).hexdigest(),outfilename)
+    f = open(outfilename, "w+")
+    f.write(json.dumps(json_out))
+    f.close()
+
+def part4b(location):
+    out = []
+    for (dirpath, dirnames, filenames) in os.walk(location, topdown=True):
+        if(dirpath not in ignore_dir):
+            for f in filenames:
+                result = os.path.join(dirpath, f)
+                #print(result)
+                #print(hashlib.sha256(open(result,'rb').read()).hexdigest())
+                nested_output(result,hashlib.sha256(open(result,'rb').read()).hexdigest(),outfilename)
+            for d in dirnames:
+                result = os.path.join(dirpath, d)
+                #print(result)
+                #print(hashlib.sha256(result.encode()).hexdigest())
+                nested_output(result,hashlib.sha256(result.encode()).hexdigest(),outfilename)
+    return json_out
+def part4(filein, location, filename):
+    f = open(filein,"r")
+    loads = f.readlines()[0]
+    f.close()
+    json_compare = json.loads(loads)
+    json_now = part4b(location)
+
+    files_found = []
+    files_added = []
+    files_deleted = []
+    files_modified = []
+
+    for i in json_compare:
+        old_deleted = True
+        for x in json_now:
+            added = False
+            if i['filename'] == x['filename']:
+                if i['hash'] == x['hash']:
+                    files_found.append(i['filename'])
+                else:
+                    files_modified.append(i['filename'])
+                old_deleted = False
+            else:
+                added = True
+            if(added):
+                files_added.append(x['filename']) 
+        if(old_deleted == True):
+            files_deleted.append(i['filename'])
+
+    f = open("RESULTS"+filename, 'a')
+    f.write("FOUND: " + json.dumps(files_found) + "\n")
+    f.write("MODIFIED: " + json.dumps(files_modified) + "\n")
+    f.write("DELETED: " + json.dumps(files_deleted) + "\n")
+    f.write("ADDED: " + json.dumps(files_added) +"\n")
+    f.close()
+
+    print("Found: " + str(len(files_found)) + " Old Files")
+    print("Found: " + str(len(files_modified)) + " Modified Files")
+    print("Found: " + str(len(files_deleted)) + " Deleted Files")
+    print("Found: " + str(len(files_added)) + " New Files")
+    print("These files have been logged in: " + "RESULTS"+filename + " for your records.")
+        #print(i)
+        #if i in json_now:
+            #print(i['filename'] + " | FOUND")
+        #if i not in json_now:
+            #print(i['filename'] + " | DELETED")
+    '''for i in json_now:
+        if i not in json_compare:
+            print(i["filename"] + " | CREATED")'''
+
 
 if __name__ == "__main__":
-    if(len(sys.argv) < 2): quit()
+    #if(len(sys.argv) < 4): quit()
     outfilename = datetime.now().strftime("%H%M%S-%d%b%Y") + ".SY402Log"
-    part3(sys.argv,outfilename)
+    if(sys.argv[1] == "-c"):
+        part4(sys.argv[2], sys.argv[3], outfilename)
+    elif(sys.argv[1] == "-s"):
+        part3(sys.argv,outfilename)
+    #part3(sys.argv,outfilename)
     #part2(sys.argv)
     #part1(sys.argv)
